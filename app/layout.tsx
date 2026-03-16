133e7b0b72f0f9e8fac03af6a8dd7607acef440f
Muchammad Ardiansyah <achardianself@gmail.com>
2026-03-16 13:30:09 +0700

    chore: initial setup

---
+++ app/layout.tsx
@@ -0,0 +1,49 @@
+// For adding custom fonts with other frameworks, see:
+// https://tailwindcss.com/docs/font-family
+import type { Metadata } from 'next';
+import { Inter, JetBrains_Mono, Source_Serif_4 } from 'next/font/google';
+
+import { cn } from '@/lib/utils';
+
+import './globals.css';
+
+const fontSans = Inter({
+  subsets: ['latin'],
+  variable: '--font-sans',
+});
+
+const fontSerif = Source_Serif_4({
+  subsets: ['latin'],
+  variable: '--font-serif',
+});
+
+const fontMono = JetBrains_Mono({
+  subsets: ['latin'],
+  variable: '--font-mono',
+});
+
+export const metadata: Metadata = {
+  title: 'ReadLog',
+  description: 'Readlog - Track and organize the books you read',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='en'>
+      <body
+        className={cn(
+          fontMono.variable,
+          fontSans.variable,
+          fontSerif.variable,
+          'antialiased'
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1 className='text-2xl font-bold underline'>Hello World</h1>
+      <Button>Click Here</Button>
+    </div>
+  );
+};
+
+export default HomePage;
